c44a1b4887a6fdedceb81d543a6464e0c70707b8
daniel.aviles <desarrollador.junior6@interedes.com.co>
2021-03-13 13:42:00 -0500

    avances

--- Servidor/routes/auth.js
+++ Servidor/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const loginC = require('../controller/authController');
+const authC = require('../controller/authController');
 
 router.post('/login', async (req, res) => {
   try {
-    await loginC.authLogin(req)
+    await authC.authLogin(req)
       .then((data) => res.json(data))
       .catch((err) => console.error(err));
   } catch (err) {
@@ -22,7 +22,7 @@ router.get('/verifyInfo', async (req, res) => {
   */
   try {
     const { q, type } = req.query;
-    await loginC.verifyData(q, type)
+    await authC.verifyData(q, type)
       .then((data) => res.json(data[0].exist))
       .catch((err) => console.error(err));
   } catch (err) {
@@ -32,7 +32,7 @@ router.get('/verifyInfo', async (req, res) => {
 
 router.post('/register', async (req, res) => {
   try {
-    await loginC.registerUser(req)
+    await authC.registerUser(req)
       .then(() => res.json({status: 1, msg: 'Success'}))
       .catch((err) => res.json({status: 0, msg: `Error ${err}`}));
   } catch (err) {
